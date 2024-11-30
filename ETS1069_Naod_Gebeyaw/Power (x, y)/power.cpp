98c61e1426750ab83ed10ffe6c649f49d937850a
Naod Gebeyaw <naod.gebeyaw@aastustudent.edu.et>
2024-11-30 07:38:12 -0800

    Update power.cpp

--- ETS1069_Naod_Gebeyaw/Power (x, y)/power.cpp
+++ ETS1069_Naod_Gebeyaw/Power (x, y)/power.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float x, y, value = 1;
+    cout << "Enter the base: ";
+    cin >> x;
+    cout << "Enter the exponent: ";
+    cin >> y;
+    for(int i = 1; i <= y; i++){
+        value *= x;
+    }
+    cout << "x^y = " << value << endl;
+    return 0;
+}
