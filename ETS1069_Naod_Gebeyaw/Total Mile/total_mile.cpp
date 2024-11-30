bd8bebf9f22bfcc4a00f09a6f4e87072bd81ec99
Naod Gebeyaw <naod.gebeyaw@aastustudent.edu.et>
2024-11-30 09:01:04 -0800

    Update total_mile.cpp

--- ETS1069_Naod_Gebeyaw/Total Mile/total_mile.cpp
+++ ETS1069_Naod_Gebeyaw/Total Mile/total_mile.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float tank_capacity, miles_per_gallon, total_miles;
+
+    cout << "Enter the fuel tank capacity in gallons: ";
+    cin >> tank_capacity;
+    if(cin.fail()){
+        cout << "ERROR! invalid input.";
+        return 0;
+    }
+    cout << "Enter the fuel efficiency in miles per gallon: ";
+    cin >> miles_per_gallon;
+    if(cin.fail()){
+        cout << "ERROR! invalid input.";
+        return 0;
+    }
+    
+    total_miles = tank_capacity * miles_per_gallon;
+
+    cout << "The automobile can be driven " << total_miles 
+         << " miles without refueling." << endl;
+
+    return 0;
+}
