f164f1e2ef9716678a1a138d180a966a002a7ae4
Nazrawit Tewodros Bezabih <nazrawitbezabih@gmail.com>
2024-12-01 09:49:24 +0300

    Update miles.cpp

--- ETS1109-Nazrawit-Tewodros/totalNumberOfMiles/miles.cpp
+++ ETS1109-Nazrawit-Tewodros/totalNumberOfMiles/miles.cpp
@@ -2,11 +2,41 @@
 using namespace std;
 int main()
 {
-    float capacity, miles_pergallon, total_miles;
-    cout << "Enter the capacity of the fuel tank in gallons: ";
-    cin >> capacity;
-    cout << "Enter the miles per gallon the automobile can be driven: ";
-    cin >> miles_pergallon;
+    float capacity, miles_pergallon, total_miles,trial=0;
+    while (trial<5){
+        cout << "Enter the capacity of the fuel tank in gallons: ";
+        cin >> capacity;
+        if (cin.fail()){
+            trial++;
+            cin.clear();
+            cin.ignore();
+            cout<<"invaild attempt"<<endl;
+        }else {
+            break;
+        }
+        if (trial==5){
+            cout<<"too many invalid attempt"<<endl;
+            return 1;
+        }
+    }
+    trial=0;
+    while (trial<5){
+        cout << "Enter the miles per gallon the automobile can be driven: ";
+        cin >> miles_pergallon;
+        if (cin.fail()){
+            trial++;
+            cin.clear();
+            cin.ignore();
+            cout<<"invaild attempt"<<endl;
+        }else {
+            break;
+        }
+        if (trial==5){
+            cout<<"too many invalid attempt"<<endl;
+            return 1;
+        }
+    }
+    
     total_miles = capacity * miles_pergallon;
     cout << "The automobile can be driven " << total_miles << " miles without refueling." << endl;
     return 0;
