a0bb098818952f03504a89dee6332de6e4cd2814
Naod Gebeyaw <naod.gebeyaw@aastustudent.edu.et>
2024-12-01 22:29:53 +0300

    Update transmission.cpp

--- ETS1069_Naod_Gebeyaw/Transmission/transmission.cpp
+++ ETS1069_Naod_Gebeyaw/Transmission/transmission.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main() {
     const int transmission_rate = 960;
-    long file_size = 419430400;
+    long file_size;
+    
+    cout <<"Enter the size of the file: ";
+    cin >> file_size;
     long total_second = file_size / transmission_rate;
     
     int day = total_second / (24*3600);
@@ -16,6 +19,6 @@ int main() {
     
     int second = total_second % 60;
     
-    cout << "Time to transmit a 400MB file: " << day <<" days, " << hour << " hour, " << minute << " minutes, " << second << " seconds." <<endl;
+    cout << "Time to transmit the given file: " << day <<" days, " << hour << " hour, " << minute << " minutes, " << second << " seconds." <<endl;
     return 0;    
 }
