b00732830e2a11f72860a92386453b33538add36
Lelisa Hailu <lelisahailu21@gmail.com>
2024-12-01 19:16:13 +0300

    Update number of miles.cpp

--- ETS0849_Lelisa_Hailu/Number of miles /number of miles.cpp
+++ ETS0849_Lelisa_Hailu/Number of miles /number of miles.cpp
@@ -1,1 +1,28 @@
+//the program that find the number of miles the automobile can be driven without refueling. By using c++
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float gallons, milesPerGallon, totalMiles;
+
+    // Input for gallons in fuel tank
+    cout << "Enter the number of gallons in the fuel tank: ";
+    cin >> gallons;
+
+    // Input for miles per gallon
+    cout << "Enter the miles per gallon (MPG) of the automobile: ";
+    cin >> milesPerGallon;
+
+    // Validate input
+    if (gallons < 0 || milesPerGallon < 0) {
+        cout << "Gallons and MPG must be non-negative numbers." << endl;
+    } else { 
+    //Calculate the total miles 
+        totalMiles = gallons * milesPerGallon;
+        cout << "The automobile can be driven for " << totalMiles << " miles without refueling." << endl;
+    }
+
+    return 0;
+}
