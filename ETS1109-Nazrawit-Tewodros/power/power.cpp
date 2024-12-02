4d6deb0579b045b5ebb4ff89cc7a3e261fcb42b9
Nazrawit Tewodros Bezabih <nazrawitbezabih@gmail.com>
2024-12-02 21:07:36 +0300

    Update power.cpp

--- ETS1109-Nazrawit-Tewodros/power/power.cpp
+++ ETS1109-Nazrawit-Tewodros/power/power.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-int main(){
-    double base, exponent, result;
-    cout << "Enter base: ";
-    cin >> base;
-    cout << "Enter exponent: ";
-    cin >> exponent;
-    reult= pow(base, exponent);
-    cout << base << " raised to the power of " << exponent << " is " << result << endl;
+int main() {
+    int base,exponent;
+    double result=1;
+    cout<<"enter the base and exponent";
+    cin>>base>>exponent;
+    if (cin.fail()){
+        cin.clear();
+        cin.ignore();
+    }
+    if (exponent <0 && base==0){
+        cout<<"result is undefined (division by zero)";
+        return 0;
+    }
+    if (exponent >0){
+    for(int i=1;i<=exponent;i++){
+        result*=base;   
+    }
+    } else if(exponent<0)  {
+        for (int i=-1;i>=exponent;i--){
+            result=result/base;
+        }  
+    } else {
+        result=1;
+    }
+    cout<<result;
     return 0;
 }
