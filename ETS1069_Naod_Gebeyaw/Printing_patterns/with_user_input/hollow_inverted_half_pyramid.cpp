732b7f283315af5efa2278c86b5154b6eccb2287
Naod Gebeyaw <naod.gebeyaw@aastustudent.edu.et>
2024-12-13 08:47:01 -0800

    Update hollow_inverted_half_pyramid.cpp

--- ETS1069_Naod_Gebeyaw/Printing_patterns/with_user_input/hollow_inverted_half_pyramid.cpp
+++ ETS1069_Naod_Gebeyaw/Printing_patterns/with_user_input/hollow_inverted_half_pyramid.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (int i = 1; i <= rows; i++){
         for (int j = 1; j <= rows; j++){
-            if ( i == 1 || j == 1 || i + j == 7)
+            if ( i == 1 || j == 1 || i + j == rows + 1)
                 cout << "* ";
             else
                 cout << "  ";
