546e028acf3eb824c63b2cab6aabc6e4eacb083b
natan asnake <natanasnake91@gmail.com>
2024-12-13 20:29:11 +0300

    Update numbers_right_triangle.cpp

--- ETS1081_Natan_Asnake/x.cpp/printing_patterns/with_user_input/numbers_right_triangle.cpp
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/with_user_input/numbers_right_triangle.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     int i,j,rows;
     cout<<"Please enter the number of rows you desire: ";
+    cin>>rows;
     for (i=1;i<=rows;i++) {
         for (j=1;j<=i;j++) {
             cout<<j;
