79012748fd4b299d2cb5c895a5a4ff409a3c73f7
natan asnake <natanasnake91@gmail.com>
2024-12-13 13:53:22 +0300

    Create inverted_half_pyramid.cpp

---
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/inverted_half_pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;
+
+    for (i=1;i<=6;i++) {
+
+        for (j=1;j<=7-i;j++) {
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+}
