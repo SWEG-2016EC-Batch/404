cd570cf943ab02e8faff3174021aab2248fd1dbf
natan asnake <natanasnake91@gmail.com>
2024-12-13 20:20:57 +0300

    Update full_pyramid.cpp

--- ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/full_pyramid.cpp
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/full_pyramid.cpp
@@ -5,7 +5,7 @@ int main() {
     int i,j,k;							
     for (i=1;i<=6;i++){
         for (j=1;j<=6-i;j++) {
-            cout<<"  ";
+            cout<<" ";
         }
     for (k=1;k<=i;k++){
         cout<<"* ";
