251cf898069a3f740902743b1ed4f83625f03c96
natan asnake <natanasnake91@gmail.com>
2024-12-13 16:52:47 +0300

    Update hollow_rectange.cpp

--- ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/hollow_rectange.cpp
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/hollow_rectange.cpp
@@ -5,9 +5,9 @@ int main() {
 int i,j,k;
     for (i=1;i<=7;i++) {
 
-        for (j=1;j<=9;j++) {
+        for (j=1;j<=8;j++) {
 
-            if (i==1||i==7 || j==1||j==9) {
+            if (i==1||i==7 || j==1||j==8) {
                 cout<<"* ";
             }else {
                 cout<<"  ";
