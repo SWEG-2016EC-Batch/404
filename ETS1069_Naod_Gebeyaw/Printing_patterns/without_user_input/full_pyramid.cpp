4932dc900d37fc072b6598543aa0ef68a9471184
Naod Gebeyaw <naod.gebeyaw@aastustudent.edu.et>
2024-12-13 18:35:47 +0300

    Create full_pyramid.cpp

---
+++ ETS1069_Naod_Gebeyaw/Printing_patterns/without_user_input/full_pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 6; i++) {
+        for (int j = 1; j <= 6 - i; j++) {
+            cout << " ";
+        }
+    for (int j = 1; j <= i; j++) {
+        cout << "*";
+        if (j < i) {
+            cout << " ";
+        }
+    }
+
+    cout << endl;
+    }
+
+    return 0;
+}
