16a8b185527beb7b6ec7a642dbf42d3736249391
natan asnake <natanasnake91@gmail.com>
2024-12-13 14:18:52 +0300

    Create inverted_full_pyramid.cpp

---
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/no_user_input/inverted_full_pyramid.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k;                                                       
+    for (i=1;i<=6;i++){
+        for (j=1;j<=i;j++) {
+            cout<<"  ";
+        }
+    for (k=1;k<=6-i;k++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+return 0;
+}
