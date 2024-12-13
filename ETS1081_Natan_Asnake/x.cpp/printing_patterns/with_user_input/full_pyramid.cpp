656bd6536cdedacdb7a58dd9dcca044e11415c20
natan asnake <natanasnake91@gmail.com>
2024-12-13 16:22:27 +0300

    Create full_pyramid.cpp

---
+++ ETS1081_Natan_Asnake/x.cpp/printing_patterns/with_user_input/full_pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,j,k,rows,cols;
+    cout<<"Please enter the number of rows you desire: "<<endl;
+    cin>>rows;
+    cout<<"please enter the number of cols you want: "<<endl;
+    cin>>cols;
+    for (i=1;i<=rows;i++){
+        for (j=1;j<=cols-i;j++) {
+            cout<<"  ";
+        }
+    for (k=1;k<=i;k++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+return 0;
+}
